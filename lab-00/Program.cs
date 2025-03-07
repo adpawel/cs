0083f4a74ce7dce955f687ed034ca412d735d615
adpawel <adamczyk@student.agh.edu.pl>
2025-03-07 22:22:05 +0100

    initial commit

---
+++ lab-00/Program.cs
@@ -0,0 +1,160 @@
+﻿using System.Data;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+
+namespace lab0{
+    public class MainClass{
+        public static void Main(string[] args){
+            string task = args[0];
+            switch(task){
+                case "task1":
+                    task1.RepeatingString.Execute();
+                    break;
+                case "task2":
+                    task2.FileWriting.Execute();
+                    break;
+                case "task3":
+                    task3.FileReading.Execute();
+                    break;
+                case "task4":
+                    task4.Music.Execute();
+                    break;
+                default:
+                    Console.WriteLine("Nie ma takiego zadania.");
+                    break;
+            }
+        }
+    }
+}
+
+namespace task1{
+    class RepeatingString{
+        public static void Execute(){            
+            Console.Write("Podaj zestaw napisów i liczbę powtórzeń: ");
+            string ?input = Console.ReadLine();
+            
+            if (string.IsNullOrWhiteSpace(input)) {
+                Console.WriteLine("Błędne dane wejściowe.");
+                return;
+            }
+            
+            List<string> words = input.Split(" ").ToList();
+            
+            string lastString = words[^1];
+            if (!int.TryParse(lastString, out int n) || n <= 0) {
+                Console.WriteLine($"'{lastString}' nie jest poprawną liczbą.");
+                return;
+            }
+
+            words.RemoveAt(words.Count - 1);
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (string word in words) {
+                for (int j = 0; j < n; j++) {
+                    output.Append(word).Append(' ');
+                }
+            }
+
+            Console.WriteLine(output.ToString().TrimEnd()); 
+        }
+    }
+}
+
+namespace task2{
+    class FileWriting{
+        public static void Execute(){
+            int count = 0;
+            int sum = 0;
+
+            while(true){
+                string? line = Console.ReadLine();
+        
+                if(!string.IsNullOrEmpty(line)){
+                    int a = int.Parse(line);
+                    if(a == 0) break;
+                    sum += a;
+                }
+                count++;
+            }
+            
+            float mean = (float) sum / count;
+            string filePath = "output.txt";
+            File.WriteAllText(filePath, "" + mean);
+            Console.WriteLine("Dane zapisane do pliku: {0}", filePath);
+        }
+    }
+}
+
+namespace task3{
+    class FileReading{
+        public static void Execute(){
+            Console.Write("Podaj nazwę pliku: ");
+            string? filePath = Console.ReadLine();
+            
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Plik nie istnieje!");
+                return;
+            }
+
+            StreamReader streamReader = new StreamReader(filePath);
+            
+            int lineNumber = 1;
+            int currentLineNumber = 1;
+            int maxNumber = int.MinValue;
+
+            while(!streamReader.EndOfStream){
+                string ?line = streamReader.ReadLine();
+                if(line == null) continue;
+
+                List<string> list = line.Split(" ").ToList();
+                foreach(string s in list){
+                    if (int.TryParse(s, out int n) && n > maxNumber) {
+                        maxNumber = n;
+                        lineNumber = currentLineNumber;
+                    }
+                }
+                currentLineNumber++;
+            }
+            streamReader.Close();
+
+            if (maxNumber == int.MinValue)
+            {
+                Console.WriteLine("Brak liczb w pliku.");
+            } else{
+                Console.WriteLine("{0}, linijka: {1}", maxNumber, lineNumber);
+            }
+        }
+    }
+}
+
+
+namespace task4{
+    public class Music{
+        public static void Execute(){
+            string[] arr = new string[]{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "B", "H"};
+            string? input = Console.ReadLine();
+            int[] leaps = new int[]{2, 2, 1, 2, 2, 2, 1};
+
+            if (Array.IndexOf(arr, input) == -1)
+            {
+                Console.WriteLine("Niepoprawny dźwięk!");
+                return;
+            }
+
+            int startIndex = Array.IndexOf(arr, input); 
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(arr[startIndex]).Append(' ');  
+
+            foreach (int n in leaps)
+            {
+                startIndex = (startIndex + n) % arr.Length;  
+                sb.Append(arr[startIndex]).Append(' ');      
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
---
+++ lab-01/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace lab1{
+    public class MainClass{
+        public static void Main(string[] args){
+            string task = args[0];
+            args = args.Skip(1).ToArray();
+            switch(task){
+                case "task2":
+                    task2.FileStringWriting.Execute();
+                    break;
+                case "task3":
+                    task3.StringFinder.Execute(args);
+                    break;
+                case "task4":
+                    task4.RNG.Execute(args);
+                    break;
+                case "task5":
+                    task5.FileOverview.Execute();
+                    break;
+                default:
+                    Console.WriteLine("Nie ma takiego zadania.");
+                    break;
+            }
+        }
+    }
+}
+
+namespace task2{
+    class FileStringWriting{
+        public static void Execute(){
+            Console.WriteLine("Podawaj napisy: ");
+            string result = "";
+
+            StreamWriter sw = new StreamWriter("output_task2.txt", append:false);
+            while(true){
+                string? napis = Console.ReadLine();
+                if(napis == "koniec!"){
+                       break;
+                }
+
+                if(!string.IsNullOrEmpty(napis)){
+                    sw.WriteLine(napis);
+                    if(string.Compare(napis, result) > 0){
+                        result = napis;
+                    }
+                }
+            }
+            sw.Close();
+
+            Console.WriteLine("Pomyślnie zapisano do pliku. Szukany wyraz: {0}", result);
+        }
+    }
+}
+
+
+namespace task3{
+    public class StringFinder{
+        public static void Execute(string[] args){
+            string filePath = args[0];
+            string toFind = args[1];
+
+            int lineNumber = 1;
+            StringBuilder sb = new StringBuilder();
+            if(!File.Exists(filePath)){
+                Console.WriteLine($"Plik {filePath} nie istnieje");
+                return;
+            }
+
+            StreamReader sr = new StreamReader(filePath);
+            while(!sr.EndOfStream){
+                string? line = sr.ReadLine();
+                if(!string.IsNullOrEmpty(line)){
+                    int position = line.IndexOf(toFind);
+
+                    while(position != -1){
+                        sb.Append($"linijka: {lineNumber}, pozycja: {position}\n");
+                        position = line.IndexOf(toFind, position + 1);
+                    }
+                }
+                lineNumber++;
+            }
+            sr.Close();
+
+            if(sb.ToString().Length > 0){
+                Console.WriteLine("Wystąpienia podanego ciągu: ");
+                Console.Write(sb.ToString());
+            }
+            else Console.WriteLine("Brak wystąpień podanego ciągu w tekście.");
+        }
+    }
+}
+
+
+namespace task4{
+    public class RNG{
+        public static void Execute(string[] args){
+            string fileName = args[0];
+            int n = int.Parse(args[1]);
+            string range = args[2];
+            int seed = int.Parse(args[3]);
+            bool isInt = Boolean.Parse(args[4]); 
+
+            int start = int.Parse(range.Split(":")[0]);
+            int end = int.Parse(range.Split(":")[1]);
+
+            Random random = new Random(seed);
+            StreamWriter sw = new StreamWriter(fileName, append:false);
+            for(int i = 0; i < n; i++){
+                if(isInt) sw.WriteLine("" + random.Next(start, end + 1));
+                else sw.WriteLine((random.NextDouble()*(end - start) + start).ToString());
+            }
+            sw.Close();
+
+            Console.WriteLine($"Pomyślnie wygenerowano {n} liczb losowych.");
+        }
+    }
+}
+
+
+namespace task5{
+    public class FileOverview{
+        public static void Execute(){
+            string filename = "output_task4.txt";
+            string[] lines = File.ReadAllLines(filename);
+            int numberOfLines = lines.Length;
+            
+            int numberOfChars = 0;
+            foreach(string line in lines){
+                numberOfChars += line.Length;
+            }
+
+            if(numberOfLines == 0){
+                Console.WriteLine("Plik jest pusty.");
+                return;
+            }
+
+            List<double> nums = new List<double>();
+            foreach(string line in lines){
+                nums.Add(double.Parse(line));
+            }
+            double minNumber = nums.Min();
+            double maxNumber = nums.Max();
+            double mean = nums.Average();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Liczba linii: {numberOfLines}");
+            sb.Append($"\nLiczba znaków: {numberOfChars}");
+            sb.Append($"\nNajwiększa liczba: {maxNumber}");
+            sb.Append($"\nNajmniejsza liczba: {minNumber}");
+            sb.Append($"\nŚrednia: {mean}");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
